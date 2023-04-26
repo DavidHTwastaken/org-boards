c984e2608f85ed1ce060479962bf35c54e51c9ce
David Houle-Tymeczko <david.houletymeczko@ontariotechu.net>
2023-04-26 01:41:34 -0400

    refactoring

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,94 +1,25 @@
 const cardsContainer = document.querySelector('.cards-container');
-const addCardBtn = document.querySelector('#add-card-btn');
+// const addCardBtn = document.querySelector('#add-card-btn');
 
-/**
- * @param socket {WebSocket}
- * */
 function setupAddCardBtn() {
-    if(socket === undefined){
-        console.error("No socket connection has been made: "+socket);
-    }
-    if(username===undefined){
-        console.error("No username: "+username);
-    }
-    console.log(socket.readyState);
+    // Create the button element
+    const addCardBtn = document.createElement('button');
+    addCardBtn.id = 'add-card-btn';
+    addCardBtn.innerText = 'Add a card';
+    cardsContainer.appendChild(addCardBtn);
+
+    // Set up functionality
     addCardBtn.addEventListener('click', () => {
-        // Create a new card
-        const newCard = document.createElement('div');
-        newCard.className = 'card';
-        newCard.id = 'title';
+        const newCard = createCard();
+        newCard.id = 'new-card';
 
-        // Create the card title input
-        const cardTitleInput = document.createElement('input');
-        cardTitleInput.type = 'text';
-        cardTitleInput.placeholder = 'Add a title...';
-        cardTitleInput.id = 'title';
-
-        // Create the card title submit button
-        const cardTitleSubmitBtn = document.createElement('button');
-        cardTitleSubmitBtn.type = 'button';
-        cardTitleSubmitBtn.textContent = 'Submit';
-        cardTitleSubmitBtn.className = 'card-title-submit-btn';
-
-        // Delete button
-        const deleteCardButton = document.createElement('button');
-        deleteCardButton.type = 'button';
-        deleteCardButton.className = 'add-message-btn';
-        deleteCardButton.textContent = 'Delete Card';
-
-        // Append the card title input and submit button to the new card
-        newCard.appendChild(cardTitleInput);
-        newCard.appendChild(cardTitleSubmitBtn);
-        newCard.appendChild(deleteCardButton);
-
-        // Add the "Add Message" button to the new card
-        const addMessageBtn = document.createElement('button');
-        addMessageBtn.textContent = 'Add Message';
-        addMessageBtn.className = 'add-message-btn';
-        newCard.appendChild(addMessageBtn);
+        // Add features to new card
+        setupTitleInput(newCard);
+        addDeleteCardBtn(newCard);
+        createAddMessageBtn(newCard);
 
         // Add the new card to the cards container
         cardsContainer.insertBefore(newCard, addCardBtn);
-
-        // Focus the card title input
-        cardTitleInput.focus();
-
-        // When the user clicks the submit button or presses Enter, create the card title
-        const createCardTitle = () => {
-            // Check if the title input is empty
-            if (cardTitleInput.value.trim() === '') {
-                cardTitleInput.value = 'Empty Title';
-            }
-
-            // Create the card title
-            const cardTitle = document.createElement('div');
-            cardTitle.className = 'card-title';
-            cardTitle.textContent = cardTitleInput.value;
-
-            // Insert the card title into the new card
-            newCard.insertBefore(cardTitle, cardTitleInput);
-
-            // Remove the title input and submit button
-            newCard.removeChild(cardTitleInput);
-            newCard.removeChild(cardTitleSubmitBtn);
-
-            // Notify socket server
-            console.log("Sending create instructions: "+JSON.stringify({"type": "new-card", "title": cardTitle.textContent,"creator": username}));
-            socket.send(JSON.stringify({"type": "new-card", "title": cardTitle.textContent,"creator": username}));
-        };
-
-        cardTitleSubmitBtn.addEventListener('click', createCardTitle);
-        cardTitleInput.addEventListener('keyup', (event) => {
-            if (event.key === 'Enter') {
-                createCardTitle();
-            }
-        });
-        deleteCardButton.addEventListener('click', deleteCard);
-        deleteCardButton.addEventListener('keyup', (event) => {
-            if (event.key === 'Enter') {
-                deleteCard(event);
-            }
-        });
 
         // When the user clicks the "Add Message" button, create a message input box
         addMessageBtn.addEventListener('click', () => {
@@ -178,14 +109,116 @@ function setupAddCardBtn() {
     });
 }
 
-const deleteCard = (event) => {
-    let nodes = event.target.parentNode.parentNode.querySelectorAll(".card")
-    for(let i = 0; i<nodes.length; i++){
-        if(nodes[i] === event.target.parentNode){
-            let request = JSON.stringify({"type":"delete-card","card":i});
-            console.log("Sending instructions: " + request);
-            socket.send(request);
+const createCard = () => {
+    // Create a new card
+    const newCard = document.createElement('div');
+    newCard.className = 'card';
+    const currentCards = cardsContainer.getElementsByClassName('card');
+    cardsContainer.insertBefore(newCard,currentCards.item(currentCards.length-1));
+    return newCard;
+}
+
+const setupTitleInput = (card) => {
+    // Create the card title input
+    const cardTitleInput = document.createElement('input');
+    cardTitleInput.type = 'text';
+    cardTitleInput.placeholder = 'Add a title...';
+
+    // Create the card title submit button
+    const cardTitleSubmitBtn = document.createElement('button');
+    cardTitleSubmitBtn.type = 'button';
+    cardTitleSubmitBtn.textContent = 'Submit';
+    cardTitleSubmitBtn.className = 'card-title-submit-btn';
+
+    // Append the card title input and submit button to the new card
+    card.appendChild(cardTitleInput);
+    card.appendChild(cardTitleSubmitBtn);
+
+    // Focus the card title input
+    cardTitleInput.focus();
+
+    // When the user clicks the submit button or presses Enter, create the card title
+    const createCardTitle = () => {
+        // Check if the title input is empty
+        if (cardTitleInput.value.trim() === '') {
+            cardTitleInput.value = 'Empty Title';
         }
+
+        // Create the card title
+        const cardTitle = document.createElement('div');
+        cardTitle.className = 'card-title';
+        cardTitle.textContent = cardTitleInput.value;
+
+        // Insert the card title into the new card
+        card.insertBefore(cardTitle, cardTitleInput);
+
+        // Remove the title input and submit button
+        card.removeChild(cardTitleInput);
+        card.removeChild(cardTitleSubmitBtn);
+
+        // Card complete
+        finalizeCard(card,cardTitle.textContent);
+    };
+
+    cardTitleSubmitBtn.addEventListener('click', createCardTitle);
+    cardTitleInput.addEventListener('keyup', (event) => {
+        if (event.key === 'Enter') {
+            createCardTitle();
+        }
+    });
+}
+
+const finalizeCard = (card,title) => {
+    // Notify socket server
+    const request = JSON.stringify(
+        {"type": "new-card", "title": title,"creator": username}
+    );
+    console.log("Sending instruction to create card: "+ request);
+    socket.send(request);
+
+    // Fix card and set up button
+    card.removeAttribute('id');
+    setupAddCardBtn();
+}
+
+const createAddMessageBtn = (card) => {
+    // Add the "Add Message" button to the new card
+    const addMessageBtn = document.createElement('button');
+    addMessageBtn.textContent = 'Add Message';
+    addMessageBtn.className = 'add-message-btn';
+    card.appendChild(addMessageBtn);
+}
+
+const addDeleteCardBtn = (card) => {
+    // Delete button
+    const deleteCardButton = document.createElement('button');
+    deleteCardButton.type = 'button';
+    deleteCardButton.className = 'card-btn';
+    deleteCardButton.textContent = 'Delete Card';
+
+    // Functionality
+    deleteCardButton.addEventListener('click', deleteCard);
+    deleteCardButton.addEventListener('keyup', (event) => {
+        if (event.key === 'Enter') {
+            deleteCard(event);
+        }
+    });
+
+    card.appendChild(deleteCardButton);
+}
+
+const deleteCard = (event) => {
+    if(event.target.parentNode.id !== 'new-card'){
+        let nodes = event.target.parentNode.parentNode.querySelectorAll(".card")
+        for(let i = 0; i<nodes.length; i++){
+            if(nodes[i] === event.target.parentNode){
+                let request = JSON.stringify({"type":"delete-card","card":i});
+                console.log("Sending instructions: " + request);
+                socket.send(request);
+            }
+        }
+    } else{
+
     }
     event.target.parentNode.remove();
 };
